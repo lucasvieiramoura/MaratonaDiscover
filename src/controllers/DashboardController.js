b7b0396b324fbccf37b5defafe638bc5491fb994
lucasvieiramoura <lucasvieiramoura@gmail.com>
2021-04-27 23:04:46 -0300

    Refactor Dashboard

---
+++ src/controllers/DashboardController.js
@@ -0,0 +1,49 @@
+const Job = require('../model/Job')
+const JobUtils = require('../utils/JobUtils')
+const Profile = require('../model/Profile')
+
+module.exports = {
+    index(req, res)  {
+    
+    const jobs = Job.get();
+    const profile = Profile.get();
+
+        let statusCount = {
+            progress: 0,
+            done: 0,
+            total: jobs.length
+        }
+
+        //total de horas por dia de cada Job em progresso
+        let jobTotalHours = 0
+
+
+        const updatejobs = jobs.map((job) =>{
+            
+            const remaining = JobUtils.remainingDays(job)
+            const status = remaining <= 0 ? 'done' : 'progress'
+
+            //statusCunt[done || progress ] soma na linha 12 ou 13 
+            // +=1  a + a = result  1 + 1 
+            statusCount[status] += 1;
+
+            // Total de horas por dia de cada job em progress
+            jobTotalHours = status == 'progress' ? jobTotalHours + Number(job["daily-hours"]) : jobTotalHours
+
+            return {
+                ...job,
+                remaining,
+                status,
+                budget: JobUtils.calculateBudget(job, profile["value-hour"])
+            }    	    
+        })
+
+        // Qts de horas que quero trabalhar por dia (profile)
+        // MENOS
+        // quantidade de horas/dia de cada job in progress
+        const freeHours = profile["hours-per-day"] - jobTotalHours;
+
+        return res.render( 'index',{jobs : updatejobs, profile : profile, statusCount: statusCount, freeHours: freeHours})
+    }
+}
+
--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -3,25 +3,6 @@ const JobUtils = require('../utils/JobUtils')
 const Profile = require('../model/Profile')
 
 module.exports = {
-    index(req, res)  {
-    
-    const jobs = Job.get();
-    const profile = Profile.get();
-
-    const updatejobs = jobs.map((job) =>{
-        
-        const remaining = JobUtils.remainingDays(job)
-        const status = remaining <= 0 ? 'done' : 'progress'
-
-        return {
-            ...job,
-            remaining,
-            status,
-            budget: JobUtils.calculateBudget(job, profile["value-hour"])
-        }    	    
-        })
-        return res.render( 'index',{jobs : updatejobs})
-    },
     create(req, res) {
         return res.render( "job")
     },
